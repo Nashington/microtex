8f63310d86182048d6c07c951da7f599ef7757cf
Nashua Mashhor <17481086+Nashington@users.noreply.github.com>
2022-12-20 23:16:40 +0000

    initial commit
    code from Dr. Bigot email 07/11/2022

---
+++ microtextures.py
@@ -0,0 +1,90 @@
+import cadquery as cq
+from cadquery import exporters
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+
+########### Functions Definitions ############
+# Add function to Rotate working plane with Angle to get different orientation of textures
+# Add function to transform an STL into a cadquery model - so we can manipulate real scans
+def create_disc(Diameter, Height, EllipseW, EllipseH, gap):
+    Disc = cq.Workplane("front").circle(Diameter / 2).extrude(Height)
+    # Change Extrusion Working Plane
+    Disc = Disc.workplane().transformed(offset=cq.Vector(0, 0, Height / 2), rotate=cq.Vector(90, 0, 0))
+    # Create Scallops
+    Disc = Disc.center(-Diameter / 2 + EllipseW + gap, 0).ellipse(EllipseW, EllipseH).cutThruAll()
+    total = 2 * EllipseW + gap
+    while total < Diameter:
+        total += 2 * EllipseW + gap
+        Disc = Disc.center(2 * EllipseW + gap, 0).ellipse(EllipseW, EllipseH).cutThruAll()
+    return Disc
+
+def reduce_area(model,Diameter,Height,AreaW) :
+    if AreaW < Diameter and AreaW != 0:
+        model = model.workplane(centerOption="CenterOfMass").center(Diameter / 4 + AreaW / 2, 0).rect(Diameter / 2, Height + 1).cutThruAll()
+        model = model.center(-Diameter / 2 - AreaW, 0).rect(Diameter / 2, Height + 1).cutThruAll()
+    return model
+
+def create_square(Diameter, Height, EllipseW, EllipseH, gap):
+    # Square version
+    Square = cq.Workplane("XY").box(Diameter, Diameter, Height).faces(">Y").workplane()
+
+    Square = Square.center(-Diameter / 2 + EllipseW + gap, 0.5).ellipse(EllipseW, EllipseH).cutThruAll()
+    total = 2 * EllipseW + gap
+    while total < Diameter:
+        total += 2 * EllipseW + gap
+        Square = Square.center(2 * EllipseW + gap, 0).ellipse(EllipseW, EllipseH).cutThruAll()
+    return Square
+
+def cutout_disc (model,Diameter) :
+    model = model.cut(cq.Workplane("XY").rect(Diameter * 3, Diameter * 3).circle(Diameter / 2).extrude(2))
+    model = model.cut(cq.Workplane("XY").rect(Diameter * 3, Diameter * 3).circle(Diameter / 2).extrude(-2))
+    return model
+
+def show_model(filename):
+    # Create a new plot
+    figure = pyplot.figure()
+    axes = mplot3d.Axes3D(figure)
+    # Load the STL files and add the vectors to the plot
+    your_mesh = mesh.Mesh.from_file(filename)
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors, edgecolor='k'))
+    # Auto scale to the mesh size
+    scale = your_mesh.points.flatten()
+    axes.auto_scale_xyz(scale, scale, scale)
+    # Show the plot to the screen
+    pyplot.show()
+
+########### Main Program Start ############
+# Part Parameters
+Diameter = 10
+Height = 1
+# Scallop Texture Parameters
+EllipseH = 0.1
+EllipseW = 0.15
+gap = 0.1
+Angle = 0
+
+model1 = create_disc(Diameter,Height,EllipseW,EllipseH,gap) # Create Disc
+exporters.export(model1, 'Disc1.stl')  # Save stl file
+show_model('Disc1.stl')
+
+AreaW = 5  # Area to be simulated to reduce computation
+model2 = reduce_area(model1,Diameter,Height,AreaW) # Reduce Area by removing solid each side
+exporters.export(model2, 'Disc2.stl') # Save stl file
+show_model('Disc2.stl')
+
+# This Square1.stl is mimicking what we would get from a surface constructed with real scan
+model3 = create_square(Diameter,Height,EllipseW,EllipseH,gap) # Create Square
+exporters.export(model3, 'Square1.stl') # Save stl file
+show_model('Square1.stl')
+
+# We should add a scaling function to scale up the real texture to test various width and heights
+# We now cut out the disc out (this is useful to cutout different scale)
+model4 = cutout_disc (model3,Diameter)
+exporters.export(model4, 'Square2.stl') # Save stl file
+show_model('Square2.stl')
+
+AreaW = 3  # Area to be simulated to reduce computation
+model4 = reduce_area(model4,Diameter,Height,AreaW) # Reduce Area by removing solid each side
+exporters.export(model4, 'Square3.stl') # Save stl file
+show_model('Square3.stl')
