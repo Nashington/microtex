482043bb8072c30083951ee965bad1621f529702
Nashua Mashhor <17481086+Nashington@users.noreply.github.com>
2023-10-06 12:34:09 +0100

    fix: esc characters in cmd string, partial fix
    - spaces still don't work, try concatenated string literals next

--- PyAnsys/SC_cmd.py
+++ PyAnsys/SC_cmd.py
@@ -7,14 +7,19 @@ import os
 def execute():
     # SpaceClaim installation Directory
     # Made a variable in case we want to give the user the choice to change the installation directory later
-    filepath = r"E:\ANSYS Inc\ANSYS Student\v232\scdm"
-
-
+    filepath = r"D:\Program Files\ANSYS Inc\ANSYS Student\v232\scdm"
+    cmd = r'cmd /c spaceclaim.exe /RunScript="D:\Work\microtex\PyAnsys\SC_script.py" /Headless=True /Splash=False /Welcome=False /ExitAfterScript=True'
+    
+    #cmd = r'cmd /c D:\"Program Files"\"ANSYS Inc"\"ANSYS Student"\v232\scdm\spaceclaim.exe /RunScript="D:\Work\microtex\PyAnsys\SC_script.py" /Headless=True /Splash=False /Welcome=False /ExitAfterScript=True'
+    #this works too but it's tricky to set the path in a way CMD will accept
+    
     os.chdir(filepath)
-    os.system('cmd /c spaceclaim.exe /RunScript="E:\Summer Placement\Summer Placement Testbed\model42_fluiddomain_topflush.py" /Headless=True /Splash=False /Welcome=False /ExitAfterScript=True"')
+    os.system(cmd)
     # must use /c instead of /k to close the prompt after execution if running terminal in vscode
     # we may want to keep SCDM open to accept more scripts without having to open a new instance
     # if so, build in a function to close SCDM after all scripts have been run
+
+    os.chdir()
 
 if __name__=='__main__':
     execute()
